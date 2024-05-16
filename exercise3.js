5fc32ffb6e6e79c7f3177c28170ade3a67b265ab
oilly18 <oilly18@hotmail.com>
2024-05-16 10:14:16 +0700

    firstcommit

---
+++ exercise3.js
@@ -0,0 +1,9 @@
+let dayNumber = 6; 
+
+if (dayNumber === 0 || dayNumber === 6) {
+  console.log("It is the weekend!");
+} else if (dayNumber > 6) { 
+  console.log("Invalid day number");
+} else { 
+  console.log("It is the weekday!");
+}
